e287dc09ca7578ec53b595751295d7b456eed384
Fighter <40054155+memset0x3f@users.noreply.github.com>
2022-08-20 15:55:27 +0800

    ready to deploy

--- express.js
+++ express.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 app.use(express.static(__dirname));
-app.listen(process.env.PORT || 3000)
+app.listen(process.env.PORT || 8080)
 // app.listen(8080, () => {
 //   console.log(`App listening at port 8080`)
 // })
