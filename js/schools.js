eeb1bb5c645d1387dc4787f29d19a7ed7a9fbc84
Fighter <40054155+memset0x3f@users.noreply.github.com>
2022-08-20 16:13:18 +0800

    ready to deploy

--- js/schools.js
+++ js/schools.js
@@ -1,11 +1,18 @@
 var schools = [
     { 'person': '曹锐', 'school': '香港大学', 'major': '工学院' },
-    { 'person': '陈钶 ker', 'school': '温州医科大学', 'major': '' },
+    { 'person': '陈钶', 'school': '温州医科大学', 'major': '' },
     { 'person': '陈世君', 'school': '多伦多大学', 'major': '' },
     { 'person': '陈润澎', 'school': '杭州电子科技大学', 'major': ''},
     { 'person': '付李想', 'school': '吉林大学', 'major': '' },
     { 'person': '何元捷', 'school': '深圳大学', 'major': '' },
+    { 'person': '金湛', 'school': '杭州电子科技大学', 'major': '' },
     { 'person': '李谞远', 'school': '浙江大学', 'major': '' },
     { 'person': '李思涵', 'school': '杭州电子科技大学', 'major': '' },
-    { 'person': '徐嘉浩', 'school': '武汉大学', 'major': '' }
+    { 'person': '徐嘉浩', 'school': '武汉大学', 'major': '' },
+    { 'person': '徐铭扬', 'school': '杭州电子科技大学', 'major': '' },
+    { 'person': '徐啸宇', 'school': '温州医科大学', 'major': '' },
+    { 'person': '徐泽羽', 'school': '中国人民大学', 'major': '' },
+    { 'person': '徐熙来', 'school': '杭州电子科技大学', 'major': '' },
+    { 'person': '余梓诚', 'school': '杭州电子科技大学', 'major': '' },
+    { 'person': '郑凯俊', 'school': '浙江大学（海宁校区）', 'major': '' }
 ];
